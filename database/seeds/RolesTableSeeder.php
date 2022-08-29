c17b98d6367fafe49dd94f82418b9c88c4a6d95a
serii <seriiburduja@gmail.com>
2022-08-29 11:20:10 +0300

    feat: migrations and seeding

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+    use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+    class RolesTableSeeder extends Seeder
+    {
+        /**
+         * Run the database seeds.
+         *
+         * @return void
+         */
+        public function run()
+        {
+            $data = [
+                [
+                    'name' => 'disabled',
+                ],
+                [
+                    'name' => 'user',
+                ],
+                [
+                    'name' => 'admin',
+                ],
+            ];
+            DB::table('roles')->insert($data);
+        }
+    }
