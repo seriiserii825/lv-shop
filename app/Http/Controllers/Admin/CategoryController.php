6cf3ca2356dee7fdb6349577b7f90499b7943e01
serii <seriiburduja@gmail.com>
2022-09-03 21:04:54 +0300

    feat: delete category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -82,6 +82,8 @@ class CategoryController extends Controller
    */
   public function destroy($id)
   {
-    //
+    $category = Category::findOrFail($id);
+    $category->delete();
+    return response()->noContent();
   }
 }
