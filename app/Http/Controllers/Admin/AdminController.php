a1fac9e2c82c49a7a675a50632bb07586bd0d487
serii <seriiburduja@gmail.com>
2022-09-03 10:11:29 +0300

    feat: admin widgets

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Order;
+use Illuminate\Support\Facades\DB;
+
 class AdminController extends AdminBaseController
 {
   public function index()
   {
-    return response()->json(['message' => 'Admin index']);
+    $orders_count = DB::table('orders')->count();
+    $products_count = DB::table('products')->count();
+    $users_count = DB::table('users')->count();
+    $categories_count = DB::table('categories')->count();
+    return response()->json([
+      'orders_count' => $orders_count,
+      'products_count' => $products_count,
+      'users_count' => $users_count,
+      'categories_count' => $categories_count,
+    ]);
   }
 }
