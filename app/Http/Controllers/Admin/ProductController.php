6bd5a0450c0ba050d518d69bb82e582a94e73238
serii <seriiburduja@gmail.com>
2022-09-08 20:48:38 +0300

    feat: create product

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductRequest;
 use App\Http\Resources\ProductResource;
 use App\Product;
 use Illuminate\Http\Request;
@@ -30,9 +31,10 @@ class ProductController extends Controller
    * @param  \Illuminate\Http\Request  $request
    * @return \Illuminate\Http\Response
    */
-  public function store(Request $request)
+  public function store(StoreProductRequest $request)
   {
-    //
+    $product = Product::create($request->validated());
+    return new ProductResource($product);
   }
 
   /**
