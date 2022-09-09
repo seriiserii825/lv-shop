d3cecf2b712aaea1546bd34c4d2fc37c8b3e1bfa
serii <seriiburduja@gmail.com>
2022-09-09 17:42:25 +0300

    feat: products seed

--- database/seeds/CategoriesSeeder.php
+++ database/seeds/CategoriesSeeder.php
@@ -16,15 +16,9 @@ class CategoriesSeeder extends Seeder
       ['id' => '1', 'title' => 'Men', 'parent_id' => '0'],
       ['id' => '2', 'title' => 'Women', 'parent_id' => '0'],
       ['id' => '3', 'title' => 'Kids', 'parent_id' => '0'],
-      ['id' => '4', 'title' => 'Pants', 'parent_id' => '1'],
       ['id' => '5', 'title' => 'Blouse', 'parent_id' => '2'],
-      ['id' => '6', 'title' => 'Pants', 'parent_id' => '3'],
-      ['id' => '7', 'title' => 'Shirts', 'parent_id' => '1'],
-      ['id' => '8', 'title' => 'Skirts', 'parent_id' => '2'],
-      ['id' => '9', 'title' => 'Shirts', 'parent_id' => '3'],
-      ['id' => '10', 'title' => 'T-shirts', 'parent_id' => '1'],
-      ['id' => '11', 'title' => 'T-shirts', 'parent_id' => '2'],
-      ['id' => '12', 'title' => 'T-shirts', 'parent_id' => '3'],
+      ['id' => '7', 'title' => 'Men Shirts', 'parent_id' => '1'],
+      ['id' => '8', 'title' => 'Women Skirt', 'parent_id' => '1'],
     ];
 
     DB::table('categories')->insert($data);
